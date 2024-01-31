279f3f87e158c92355d8db8e3496ad3eb95d8eae
alishirzad <ali.shirzadg@gmail.com>
2024-01-31 04:33:44 +0330

    Refactor hero section layout and add dynamic images

--- apps/web/containers/home/hero/index.tsx
+++ apps/web/containers/home/hero/index.tsx
@@ -1,20 +1,53 @@
 "use client";
 import { FC } from "react";
 import classes from "./index.module.scss";
-import ImageCombo from "./ImageCombo";
+import Image from "next/image";
 
 export const Hero: FC = () => {
   return (
     <div className={classes.hero}>
       <div className={classes.hero__title__wrapper}>
-        <span>Wanna get the job?</span>
-        <span className={classes.message}>
+        <div>Wanna get the job?</div>
+        <div className={classes.message}>
           Build a <span className={classes.blue}>Bright</span> resume
-        </span>
-        <button>Get Started</button>
+        </div>
+        <div>
+          <button>Get Started</button>
+        </div>
       </div>
       <div className={classes.hero__image}>
-        <ImageCombo />
+        <div className={classes.imageCombo}>
+          <div className={classes.imageCombo__image}>
+            <Image
+              src="/assets/image/sample-1.png"
+              alt="Hero image"
+              fill
+              typeof="svg"
+              sizes="100%"
+              style={{ zIndex: "1" }}
+            />
+          </div>
+          <div className={classes.imageCombo__image}>
+            <Image
+              src="/assets/image/sample-2.png"
+              alt="Hero image"
+              fill
+              typeof="svg"
+              sizes="100%"
+              style={{ zIndex: "2" }}
+            />
+          </div>
+          <div className={classes.imageCombo__image}>
+            <Image
+              src="/assets/image/sample-3.png"
+              alt="Hero image"
+              fill
+              typeof="svg"
+              sizes="100%"
+              style={{ zIndex: "3" }}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
